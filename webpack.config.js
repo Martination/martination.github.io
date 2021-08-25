f87f511a9a4573bbc8669c21c67a151e62f29448
Martin <martingreen@outlook.com>
2021-08-24 23:34:15 -0700

    Add my own images and add Projects text

--- webpack.config.js
+++ webpack.config.js
@@ -67,8 +67,15 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|pdf|docx?|xlsx?|csv|pptx?)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.(pdf|docx?|xlsx?|csv|pptx?)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext][query]'
+        }
       },
     ],
   },
